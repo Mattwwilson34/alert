13de8370d1d7ad15baf228ab6a86cfdb9bcc0e1f
Matt Wilson <mattwwilson34@gmail.com>
2022-03-23 21:41:30 -0400

    Added uniqid

--- src/Components/Alert/__test__/Alert.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Alert from '../Alert';
-
-describe('Alert component', () => {
-  it('Does something...', () => {
-
-  });
-});
