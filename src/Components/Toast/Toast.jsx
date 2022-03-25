72225cab5fcb2ff93d57600a8227d90bf300bee4
Matt Wilson <mattwwilson34@gmail.com>
2022-03-24 21:25:33 -0400

    Refactored with useInterval hook diff --git a/src/Components/Toast/Toast.jsx b/src/Components/Toast/Toast.jsx index ac7f766..6012c86 100644 --- a/src/Components/Toast/Toast.jsx +++ b/src/Components/Toast/Toast.jsx @@ -1,37 +1,30 @@  import '../App/App.css';  import './Toast.css'; -import React, { useEffect, useState, useCallback } from 'react'; +import useInterval from '../useInterval';
     export default function Toast(props) {
       const { toastList, setToastList } = props;
    -  const { type, message, id, timeout } = props.toastMessage;
    -  const [visible, setVisible] = useState(true);
    +  const { type, message, id, delay } = props.toastMessage;
    
    -  const deleteToast = useCallback(() => {
    +  // Deletes toast from state array
    +  const deleteToast = () => {
         const toasts = toastList.filter((toastMessage) => toastMessage.id !== id);
         setToastList([...toasts]);
    -  }, [toastList, setToastList, id]);
    +  };
    
    -  useEffect(() => {
    -    const timer = setTimeout(() => {
    -      setVisible(false);
    -      deleteToast();
    -    }, timeout);
    -    return () => clearTimeout(timer);
    -  }, [timeout, deleteToast]);
    +  // Custom hook prevents setInterval from reseting on toat rerender
    +  useInterval(() => {
    +    deleteToast();
    +  }, delay);
    
    -  if (visible) {
    -    return (
    -      <>
    -        <div className={`Toast__Container ${type}`}>
    -          <div className='Toast__Message'>{message}</div>
    -          <div className='Toast__Close' onClick={deleteToast}>
    -            x
    -          </div>
    +  return (
    +    <>
    +      <div className={`Toast__Container ${type}`}>
    +        <div className='Toast__Message'>{message}</div>
    +        <div className='Toast__Close' onClick={deleteToast}>
    +          <span className='Toast__CloseIcon'>&#10005;</span>
             </div>
    -      </>
    -    );
    -  } else {
    -    return null;
    -  }
    +      </div>
    +    </>
    +  );
     }

--- src/Components/Toast/Toast.jsx
+++ src/Components/Toast/Toast.jsx
@@ -1,37 +1,30 @@
 import '../App/App.css';
 import './Toast.css';
-import React, { useEffect, useState, useCallback } from 'react';
+import useInterval from '../useInterval';
 
 export default function Toast(props) {
   const { toastList, setToastList } = props;
-  const { type, message, id, timeout } = props.toastMessage;
-  const [visible, setVisible] = useState(true);
+  const { type, message, id, delay } = props.toastMessage;
 
-  const deleteToast = useCallback(() => {
+  // Deletes toast from state array
+  const deleteToast = () => {
     const toasts = toastList.filter((toastMessage) => toastMessage.id !== id);
     setToastList([...toasts]);
-  }, [toastList, setToastList, id]);
+  };
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setVisible(false);
-      deleteToast();
-    }, timeout);
-    return () => clearTimeout(timer);
-  }, [timeout, deleteToast]);
+  // Custom hook prevents setInterval from reseting on toat rerender
+  useInterval(() => {
+    deleteToast();
+  }, delay);
 
-  if (visible) {
-    return (
-      <>
-        <div className={`Toast__Container ${type}`}>
-          <div className='Toast__Message'>{message}</div>
-          <div className='Toast__Close' onClick={deleteToast}>
-            x
-          </div>
+  return (
+    <>
+      <div className={`Toast__Container ${type}`}>
+        <div className='Toast__Message'>{message}</div>
+        <div className='Toast__Close' onClick={deleteToast}>
+          <span className='Toast__CloseIcon'>&#10005;</span>
         </div>
-      </>
-    );
-  } else {
-    return null;
-  }
+      </div>
+    </>
+  );
 }
